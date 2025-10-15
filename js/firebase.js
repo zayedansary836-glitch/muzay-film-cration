95f03fd5deb79015367f7a347c33f5862c53f4b5
zayedansary836-glitch <zayedansary836@gmail.com>
2025-10-15 20:03:18 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,22 @@
-// firebase.js
+// /js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBH3UcpeD-a-ZEHhghi6bTkGnO9vKeu22c",
   authDomain: "muzaya-film-creation.firebaseapp.com",
   projectId: "muzaya-film-creation",
-  storageBucket: "muzaya-film-creation.firebasestorage.app",
+  storageBucket: "muzaya-film-creation.appspot.com", // corrected domain format
   messagingSenderId: "466440504989",
   appId: "1:466440504989:web:ee937abfd4dae2f1ae3674",
   measurementId: "G-02EG3SHXKC"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
