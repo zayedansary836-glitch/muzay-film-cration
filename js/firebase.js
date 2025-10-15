f04c50634d81b4d40ebef67f323e7cc4c6b52219
zayedansary836-glitch <zayedansary836@gmail.com>
2025-10-16 00:06:33 +0530

    Upgrade Firebase SDK and implement Realtime Database

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,37 @@
-// /js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
 
+// YOUR Firebase config (you provided these earlier)
 const firebaseConfig = {
   apiKey: "AIzaSyBH3UcpeD-a-ZEHhghi6bTkGnO9vKeu22c",
   authDomain: "muzaya-film-creation.firebaseapp.com",
+  databaseURL: "https://muzaya-film-creation-default-rtdb.firebaseio.com",
   projectId: "muzaya-film-creation",
-  storageBucket: "muzaya-film-creation.appspot.com", // corrected domain format
+  storageBucket: "muzaya-film-creation.firebasestorage.app",
   messagingSenderId: "466440504989",
   appId: "1:466440504989:web:ee937abfd4dae2f1ae3674",
   measurementId: "G-02EG3SHXKC"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
 
-export { app, auth, db, storage };
+// listen dashboard node (realtime)
+const dashboardRef = ref(db, 'dashboard/');
+onValue(dashboardRef, (snap) => {
+  const data = snap.val();
+  if (!data) return;
+  const setText = (id, v) => {
+    const el = document.getElementById(id);
+    if (el) el.innerText = v ?? 0;
+  };
+  setText('projectCount', data.projects);
+  setText('creatorCount', data.creators);
+  setText('photoCount', data.photos);
+  setText('videoCount', data.videos);
+});
